08e9a9d00e9a7b07bec960166beb5b925e1842c9
krithikasuresh <krithikaasureshh@gmail.com>
2016-10-29 19:58:54 -0500

    added timer and page update

--- data-1.py
+++ data-1.py
@@ -1,155 +1,128 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[210]:
 
 from bs4 import BeautifulSoup
 import requests
 import json
 import re
-
-
-# In[2]:
-
-r = requests.get("http://services.swpc.noaa.gov/text/aurora-nowcast-map.txt")
-
-
-# In[3]:
-
-soup = BeautifulSoup(r.text, 'lxml')
-
-
-# In[4]:
-
-text = soup.find('p').text
-
-
-# In[5]:
-
-regex = r" +"
-lines = text.split('\n')
-
-
-# In[6]:
-
-i = 0
+import time
+starttime=time.time()
 while True:
-    if lines[i].strip()[0] != "#":
-        break
-    i += 1
-    
+    print "tick"
+    # In[211]:
+
+    r = requests.get("http://services.swpc.noaa.gov/text/aurora-nowcast-map.txt")
 
 
-# In[7]:
+    # In[212]:
 
-data_lines = lines[i:-1]
-data = list(map(lambda x: re.split(regex, x)[1:], data_lines))
-
-print len(data)
+    soup = BeautifulSoup(r.text, 'lxml')
 
 
-# In[8]:
+    # In[213]:
 
-#checks if there are any sets that aren't 1024 values long (so shouldn't show anything)
-for i in data:
-    if len(i) != 1024:
-        print(i)
-        print(len(i))
-        break
+    text = soup.find('p').text
 
 
-# In[9]:
+    # In[214]:
 
-#latitude inc, longitude inc, points array to hold data
-lat_inc = 0.3515625
-long_inc = 0.32846715
-points = []
+    regex = r" +"
+    lines = text.split('\n')
 
 
-# In[10]:
+    # In[215]:
 
-for i, group in enumerate(data):
-    for j in group:
-        points.append((-90 + lat_inc*i, 0, j))
-print points[2:5]
-print len(points)/1024
-
-
-# In[11]:
-
-mult = 0
-#hold the correctly formatted array of data 
-new_points = []
-for i, p in enumerate(points):
-    if i and i % 1024 == 0:
-        mult = 0
-    new_points.append([p[0], -180 + long_inc*mult, p[2]])
-    mult += 1
-
-print new_points[1:10]
-print len(new_points)/1024
-print new_points[1][2]
+    i = 0
+    while True:
+        if lines[i].strip()[0] != "#":
+            break
+        i += 1
 
 
-# In[12]:
+    # In[216]:
 
-zeros = 0
-nothing = 0
-count = 0
-clean_data = []
-
-for i, p in enumerate(new_points):
-    if i and i % 1024 == 0:
-        mult = 0
-    if float(p[2]) > 5:
-        clean_data.append([int(p[0]), int(p[1]), float(p[2])])
-    else:
-        zeros += 1
-    mult += 1
+    data_lines = lines[i:-1]
+    data = list(map(lambda x: re.split(regex, x)[1:], data_lines))
 
 
-#for i in range(len(new_points)):
- #   if float(new_points[i][2]) > 0:
-#      if (int(new_points[i][1]) != int(new_points[i+1][1])):
-   #         if i % 2 == 0:
-    #            clean_data = clean_data + [int(new_points[i][0]), int(new_points[i][1]), (float(new_points[i][2])/100)]
-    #else:
-     #   zeros += 1
-        #print new_points[i][2]
+    # In[217]:
 
-print zeros
-#print nothing
-#print nothing + zeros
-print ((len(clean_data) + zeros))/1024
-#print len(clean_data) + zeros
-print len(clean_data)
+    #latitude inc, longitude inc, points array to hold data
+    lat_inc = 0.3515625
+    long_inc = 0.3515625
+    points = []
 
 
-# In[18]:
+    # In[218]:
 
-cleaner_data = []
-for i in range(len(clean_data)):
-    if i % 5 == 0:
-        cleaner_data = cleaner_data + [clean_data[i][0], clean_data[i][1], (clean_data[i][2]/100)]
-
-final_data = [["Aurora Borealis View Prediction", cleaner_data]]
-
-print len(cleaner_data)
-print len(final_data)
+    i = 0
+    for i, group in enumerate(data):
+        for j in group:
+            points.append([-90 + lat_inc*i, 0, j])
+    print points[523270]
+    print len(points)
 
 
-# In[17]:
+    # In[219]:
 
-with open('cleanAuroraData.json', 'w') as outfile:
-    json.dump(final_data, outfile)
+    mult = 0
+    #hold the correctly formatted array of data 
+    new_points = []
+    for i, p in enumerate(points):
+        if i % 1024 == 0:
+            mult = 0
+        new_points.append([p[0], -180 + long_inc*mult, p[2]])
+        mult += 1
+
+    #print new_points[52480:]
+    print mult
+    print len(new_points)
 
 
-# In[ ]:
+    # In[220]:
+
+    zeros = 0
+    clean_data = []
+    new_data = []
+    mult = 0
+
+    for i, p in enumerate(new_points):
+        if i % 1024 == 0:
+            mult = 0
+        if float(p[2]) > 5:
+            new_data.append([int(p[0]), int(p[1]), (float(p[2])/100)])
+        else:
+            zeros += 1
+        mult += 1
+
+    for i, p in enumerate(new_data):
+        if i and i % 1024 == 0:
+            mult = 0
+        if mult % 5 == 0:
+            clean_data.append([p[0], p[1], p[2]])
+        mult += 1
+
+    print len(clean_data)
+    print len(new_data)
 
 
+    # In[221]:
+
+    cleaner_data = []
+    for i in range(len(clean_data)):
+        cleaner_data = cleaner_data + [clean_data[i][0], clean_data[i][1], clean_data[i][2]]
+
+    final_data = [["Aurora Borealis View Prediction", cleaner_data]]
+
+    print len(cleaner_data)
+    print len(final_data)
 
 
-# In[ ]:
+    # In[171]:
 
-
-
+    with open('cleanAuroraData.json', 'w') as outfile:
+        json.dump(final_data, outfile)
+        
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
